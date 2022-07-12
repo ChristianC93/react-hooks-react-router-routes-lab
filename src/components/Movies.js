aaa29c58b2f9160786dfb6c4ee42879cbd12c75c
Christian Castillo <castillochristian3@gmail.com>
2022-07-12 14:52:28 -0400

    commit tree 5dab37b2070da4ffbbc7fd2ee60774bcc6109f4e

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesList = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <ul>Genres: {movie.genres.map((genre) => {
+          return (
+            <li key={genre}>
+              {genre}
+            </li>
+          )
+        })}
+        </ul>
+      </div>
+    )
+  })
+
+  
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesList}
+    </div>
+  );
 }
 
 export default Movies;
